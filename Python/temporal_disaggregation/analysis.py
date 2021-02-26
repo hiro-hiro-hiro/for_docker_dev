15250caf29310f04b72c1263bf24025b6aa5fb94
Hiroki Ishihara <ishiharahiroki0622@gmail.com>
2021-02-26 05:27:48 +0000

    no change

--- Python/temporal_disaggregation/analysis.py
+++ Python/temporal_disaggregation/analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from timedisagg.td import TempDisagg
 
-expected_dataset = pd.read_csv("sample_data.csv")
+expected_dataset = pd.read_csv("./sample_data.csv")
 
 td_obj = TempDisagg(conversion="sum", method="chow-lin-maxlog")
 final_disaggregated_output = td_obj(expected_dataset)
